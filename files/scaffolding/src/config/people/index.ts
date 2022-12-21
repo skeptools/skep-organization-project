d3af95f62df50e4f678898a50b402919847524ff
Ryan Martin <ryan@vestahealthcare.com>
2022-12-21 17:14:42 -0500

    fix: Automate some things

---
+++ files/scaffolding/src/config/people/index.ts
@@ -0,0 +1,2 @@
+export * from './janeDoe'
+export * from './johnDoe'
--- src/skep-organization-project.ts
+++ src/skep-organization-project.ts
@@ -1,15 +1,21 @@
 import * as path from 'path';
 import { CdktfProject, CdktfProjectOptions } from '@rlmartin-projen/cdktf-project';
-import { addFiles, allCases, loadSettings } from '@rlmartin-projen/projen-project';
+import { addFiles, allCases, loadSettings, squashPackages } from '@rlmartin-projen/projen-project';
 
 export interface SkepOrganizationProjectOptions extends CdktfProjectOptions {
   readonly organizationName: string;
 }
 
+const dependencies = [
+  '@skeptools/skep@~0',
+  'timezone-enum@~1',
+];
+
 export class SkepOrganizationProject extends CdktfProject {
   constructor(options: SkepOrganizationProjectOptions) {
     const tempOptions = {
       ...options,
+      deps: squashPackages([...dependencies, ...(options.deps || [])]),
       _organizationName: allCases(options.organizationName),
     };
     const { options: projectOpts, files } = loadSettings(tempOptions, path.join(__dirname, '../files'), true);
